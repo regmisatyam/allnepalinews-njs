282f06aa933faa27652abdee0022072e3ee4c6f0
SATYAM <satyamregmi@SATYAMs-Air.lan>
2025-02-18 07:56:46 -0500

    Added light/dark toggle

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { ModeToggle } from "./Toggle";
 
 const NavBar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -17,7 +18,7 @@ const NavBar = () => {
                     src="/ANN.png"
                     alt="Logo"
                     width={32}
-                    height={32} 
+                    height={32}
                     className="h-8 rounded-full"
                 />
                 <h1 className="text-2xl text-white font-bold">
@@ -27,7 +28,7 @@ const NavBar = () => {
 
             <div
                 id="menu"
-                className={`fixed md:relative top-0 right-0 h-full md:h-auto w-64 md:w-auto bg-blue-800 md:bg-transparent text-center md:text-left transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:transform-none md:transition-none`}
+                className={`fixed md:relative top-0 right-0 z-10 h-full md:h-auto w-64 md:w-auto bg-blue-800 md:bg-transparent text-center md:text-left transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out md:transform-none md:transition-none`}
             >
                 <button onClick={toggleMenu} className="md:hidden close-button-gradient block text-white font-bold px-5 py-3 m-1 rounded-full absolute top-0 right-0">
                     X
@@ -60,12 +61,14 @@ const NavBar = () => {
                     DOWNLOAD APP
                 </Link>
             </div>
-
-            <button id="menu-btn" className="md:hidden block text-white focus:outline-none" onClick={toggleMenu}>
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-            </button>
+            <div className="flex gap-3">
+                {/* <ModeToggle /> */}
+                <button id="menu-btn" className="md:hidden block text-white focus:outline-none" onClick={toggleMenu}>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
         </nav>
     );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/Nav";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 `}
       >
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
           <NavBar />
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
